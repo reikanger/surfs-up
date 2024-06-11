7a84f04cff1686acd14fdcbc4692e69486431a69
Ryan Eikanger <reikanger@gmail.com>
2024-06-10 22:11:24 -0500

    added precipitation API call

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -6,7 +6,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask
+import datetime as dt
+
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
@@ -43,8 +45,15 @@ dates_prcps = session.query(measurement).\
     filter(measurement.date >= one_year_from_recent_dt, measurement.date <= most_recent_date)\
     .with_entities(measurement.date, measurement.prcp).all()
 
-print(type(dates_prcps))
-print(dates_prcps)
+precips_dict = dict(dates_prcps)
+
+##### Stations Query #####
+
+##### TOBS Query #####
+
+##### Start Query #####
+
+##### Start End Query #####
 
 # close the database session
 session.close()
@@ -72,18 +81,7 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Find the most recent date in the data set.
-    most_recent_date = session.query(func.max(measurement.date)).scalar()
-
-    # Calculate the date one year from the last date in data set.
-    most_recent_dt = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
-    days = 30 * 12
-    one_year_from_recent_dt = most_recent_dt - dt.timedelta(days=days)
-
-    # Perform a query to retrieve the data and precipitation scores
-    dates_prcps = session.query(measurement).\
-        filter(measurement.date >= one_year_from_recent_dt, measurement.date <= most_recent_date)\
-        .with_entities(measurement.date, measurement.prcp).all()
+    return jsonify(precips_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
